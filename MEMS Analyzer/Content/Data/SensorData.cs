9a5f05854f201df0c613dc3827419e2f30b4fbb6
Heiko Rothe <mkerix@outlook.com>
2014-03-13 14:22:13 +0100

    Added view models and driver

---
+++ MEMS Analyzer/Content/Data/SensorData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEMS_Analyzer.Content.Data
+{
+    public class SensorData
+    {
+        public int id { get; set; }
+        public float accelX { get; set; }
+        public float accelY { get; set; }
+        public float accelZ { get; set; }
+        public float gyroX { get; set; }
+        public float gyroY { get; set; }
+        public float gyroZ { get; set; }
+        public float magnetoX { get; set; }
+        public float magnetoY { get; set; }
+        public float magnetoZ { get; set; }
+        public float airPressure { get; set; }
+        public float airTemp { get; set; }
+    }
+}
---
+++ MEMS Analyzer/Content/Data/SensorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEMS_Analyzer.Content.Data
+{
+    class SensorViewModel
+    {
+        public ObservableCollection<SensorData> Items { get; set; }
+    }
+}
